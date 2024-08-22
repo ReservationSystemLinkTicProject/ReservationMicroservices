181416d42af5c6e5130ee599c58aa9afad0dea3e
jonathan tellez <jonathanbtellez@gmail.com>
2024-08-21 21:33:15 -0500

    create microservice reservations && create the initial application properties

---
+++ microservice-reservations/src/main/java/com/linktic/microservice_reservations/MicroserviceReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.microservice_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReservationsApplication.class, args);
+	}
+
+}
