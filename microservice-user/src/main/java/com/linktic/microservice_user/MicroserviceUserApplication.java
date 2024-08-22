055ce161f368d6c876f7db884ae99cf712d576fb
jonathan tellez <jonathanbtellez@gmail.com>
2024-08-21 21:59:31 -0500

    fix application properties of reservations

--- microservice-user/src/main/java/com/linktic/microservice_user/MicroserviceUserApplication.java
+++ microservice-user/src/main/java/com/linktic/microservice_user/MicroserviceUserApplication.java
@@ -2,7 +2,9 @@ package com.linktic.microservice_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceUserApplication {
 
