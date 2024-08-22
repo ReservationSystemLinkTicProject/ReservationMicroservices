b6c6a6302e0970618b41c222f847532704474a90
jonathan tellez <jonathanbtellez@gmail.com>
2024-08-22 11:57:20 -0500

    configure api gateway to make the entry point of application

--- microservice-reservations/src/main/java/com/linktic/microservice_reservations/client/IServiceClient.java
+++ microservice-reservations/src/main/java/com/linktic/microservice_reservations/client/IServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-services", url = "http://localhost:8092/api/services", configuration = FeignConfig.class)
+@FeignClient(name = "msvc-services", url = "http://localhost:8080/api/services", configuration = FeignConfig.class)
 public interface IServiceClient {
     @GetMapping("/{id}")
     ServiceDto findById(@PathVariable Long id);
--- microservice-reservations/src/main/java/com/linktic/microservice_reservations/client/IUserClient.java
+++ microservice-reservations/src/main/java/com/linktic/microservice_reservations/client/IUserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-users", url = "http://localhost:8090/api/users", configuration = FeignConfig.class)
+@FeignClient(name = "msvc-users", url = "http://localhost:8080/api/users", configuration = FeignConfig.class)
 public interface IUserClient {
     @GetMapping("/{id}")
     UserDto findById(@PathVariable Long id);
