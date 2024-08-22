e222a5f929b3c4ac7b87904f9e30f7413aec3d02
jonathan tellez <jonathanbtellez@gmail.com>
2024-08-21 21:30:47 -0500

    create microservice services && create the initial application properties

---
+++ microservice-services/src/test/java/com/linktic/microservice_services/MicroserviceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.microservice_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
